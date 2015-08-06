03bdd3cc62020deee34b4d4789fc1ffcff51cc9f
Gonzalo Escribano <gescribano@gmail.com>
2015-08-06 02:29:44 -0300

    Building a simple home page

--- wordpress/wp-content/themes/retrouve/index.php
+++ wordpress/wp-content/themes/retrouve/index.php
@@ -2,16 +2,10 @@
 
 <?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
 
-  <div class="wrapper">
-  
-    <div class="title">
-      <h1><?php the_title(); ?></h1>
-    </div>
+  <h1><?php the_title(); ?></h1>
 
-    <div class="content">
-      <?php the_content(); ?>
-    </div>
-
+  <div class="content">
+    <?php the_content(); ?>
   </div>
 
 <?php endwhile;endif; ?>
